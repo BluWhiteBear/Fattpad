d4e40e35c6d2c3d934665c8a669cf28c2c30c955
Blü <bluwhitebear@gmail.com>
2026-02-03 00:20:37 -0500

    updated logo

--- scripts/popComponents.js
+++ scripts/popComponents.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
 <nav class="navbar">
 	<div class="navbar-logo">
 		<a href="index.html">
-			<img src="img/fattpad_logo_1.png" alt="Fattpad Logo" class="logo-img" style="height: 40px;">
+			<img src="img/fattpad_logo_1.png" alt="Fattpad Logo" class="logo-img">
 		</a>
 	</div>
 	<ul class="navbar-links">
